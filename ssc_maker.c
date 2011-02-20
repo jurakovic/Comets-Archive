9d502ed0e6b845bd062796432e0be47c4cad1841
jurakovic <17744091+jurakovic@users.noreply.github.com>
2011-02-20 22:16:04 +0100

    add input_nasa

--- ssc_maker.c
+++ ssc_maker.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define MAX 1000
+#define MAX 5000
 
 void  input_cfw ();
 void  input_ciel ();
+void  input_nasa ();
 float compute_period (float q, float e);
 int   compute_epoch (int y, int m, int d);
 char *edit_name (char *name);
@@ -21,14 +22,16 @@ int main () {
 		printf(" ===========================\n\n");
 		printf("  - 1 -   Comet for Windows\n");
 		printf("  - 2 -   Cartes du Ciel\n");
-		printf("  - 3 -   Kraj programa\n\n");
+		printf("  - 3 -   NASA\n");
+		printf("  - 4 -   Kraj programa\n\n");
 		printf(" Izbor: ");
 		scanf("%d", &a);
-	} while (a<1 || a>3);
+	} while (a<1 || a>4);
 
 	if (a==1) input_cfw ();
-	if (a==2) input_ciel ();
-	else if (a==3) printf("\n Kraj programa...");
+	else if (a==2) input_ciel ();
+	else if (a==3) input_nasa ();
+	else if (a==4) printf("\n Kraj programa...");
 
 	getch();
 	return 0;
@@ -126,7 +129,7 @@ void input_cfw () {
 
 	fclose(fin);
 
-	printf("\n Datoteka %s je uspjesno spremljena\n", fout_name);
+	printf("\n Uspjesno je spremljeno %d kometa u datoteci %s\n", N, fout_name);
 	printf("\n Kraj programa...");
 }
 
@@ -173,7 +176,7 @@ void input_ciel () {
 
 	for (i=0; i<MAX; i++) {
 
-		fscanf(fin, "%39c %*c %d %*c %4d %2d \
+		fscanf(fin, "%40c %*c %d %*c %4d %2d \
 				%2d %*c %d %*c %f %*c %f %*c \
 				%f %*c %f %*c %f %40[^\n]%*c",
 				comet[i].name, &comet[i].equinox, &comet[i].epoch_y, &comet[i].epoch_m,
@@ -200,7 +203,79 @@ void input_ciel () {
 			   comet[i].incl, comet[i].asc_node, comet[i].peri_node, comet[i].epoch_JD,
 			   comet[i].epoch_y, comet[i].epoch_m, comet[i].epoch_d, comet[i].epoch_h);
 
-	printf("\n Datoteka %s je uspjesno spremljena\n", fout_name);
+	printf("\n Uspjesno je spremljeno %d kometa u datoteci %s\n", N, fout_name);
+	printf("\n Kraj programa...");
+}
+
+void  input_nasa (){
+
+	int i, N=0;
+	char fin_name [80+1], fout_name [80+1];
+	FILE *fin;
+
+	struct data {
+		char name [39+1];		// 39 znakova + \0
+		int equinox;
+		int epoch_JD;
+		int epoch_y;
+		int epoch_m;
+		int epoch_d;
+		int epoch_h;
+		float peri;
+		float ecc;
+		float peri_node;
+		float asc_node;
+		float incl;
+		float period;
+		char nepotrebno [29+1];
+	} comet[MAX];
+
+	system("CLS");
+	printf("\n     Pretvaram \"NASA format\"\n");
+	printf(" ==============================\n\n");
+
+	unos2: printf(" Unesite ime izvorisne datoteke: ");
+	scanf("%s", fin_name);
+
+	fin=fopen(fin_name, "r");
+	if (fin==NULL) {
+		printf("\n Greska pri otvaranju datoteke %s\n\n", fin_name);
+		goto unos2;
+	}
+
+	else printf("\n Datoteka %s je uspjesno otvorena\n", fin_name);
+
+	printf("\n Unesite ime odredisne datoteke: ");
+	scanf("%s", fout_name);
+
+	for (i=0; i<MAX; i++) {
+
+		fscanf(fin, "%42c %d %f %f %f %f %f %4d%2d%2d.%4d %15[^\n]%*c",
+			comet[i].name, &comet[i].equinox, &comet[i].peri, &comet[i].ecc,
+			&comet[i].incl, &comet[i].peri_node, &comet[i].asc_node, &comet[i].epoch_y,
+			&comet[i].epoch_m, &comet[i].epoch_d, &comet[i].epoch_h, &comet[i].nepotrebno);
+
+		comet[i].period = compute_period (comet[i].peri, comet[i].ecc);
+		comet[i].epoch_JD = compute_epoch (comet[i].epoch_y, comet[i].epoch_m, comet[i].epoch_d);
+		edit_name (comet[i].name);
+
+		if (comet[i].ecc >= 1.000000)
+			comet[i].ecc = 0.999999;
+
+		if (comet[i].epoch_m == 0 && comet[i].epoch_d == 0 && comet[i].epoch_h == 0)
+			break;
+
+		N++;
+	}
+
+	fclose(fin);
+
+	for (i=0; i<N; i++)
+		output (fout_name, comet[i].name, comet[i].period, comet[i].peri, comet[i].ecc,
+			   comet[i].incl, comet[i].asc_node, comet[i].peri_node, comet[i].epoch_JD,
+			   comet[i].epoch_y, comet[i].epoch_m, comet[i].epoch_d, comet[i].epoch_h);
+
+	printf("\n Uspjesno je spremljeno %d kometa u datoteci %s\n", N, fout_name);
 	printf("\n Kraj programa...");
 }
 
@@ -233,7 +308,16 @@ int compute_epoch (int y, int m, int d) {
 
 char *edit_name (char *name) {
 
-	int i;
+	int i, j;
+
+	for (i=0; i<4; i++) {
+		if (name[i]==' '){
+			for (j=0; name[j+1]!='\0'; j++){
+				name[j]=name[j+1];
+			}
+			--i;
+		}
+	}
 
 	for (i=0; name[i+1]!='\0'; i++) {
 		if (name[i]==' ' && name[i+1]==' ') {
@@ -244,6 +328,7 @@ char *edit_name (char *name) {
 		if (name[i]=='/')
 			name[i]=' ';
 	}
+
 }
 
 void output (char *fout_, char *name, float P, float q, float e, float i,
