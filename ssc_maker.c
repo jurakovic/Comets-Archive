17d81a45c483ee0873782c4f352df894ad595ed7
jurakovic <17744091+jurakovic@users.noreply.github.com>
2011-02-23 15:12:48 +0100

    rename input_nasa to input_nasa_elem, add input_nasa_sbdb

--- ssc_maker.c
+++ ssc_maker.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
 #define MAX 5000
 
 int input_mpc ();
 int input_skymap ();
 int input_cfw ();
-int input_nasa ();
+int input_nasa_sbdb ();
+int input_nasa_elem ();
 int input_ciel ();
 float compute_period (float q, float e);
 int compute_epoch (int y, int m, int d);
@@ -22,18 +23,19 @@ int main (){
 		fflush(stdin);
 		fflush(stdout);
 		system("CLS");
-		printf("\n             SSC MAKER\n\n");
-		printf(" ==================================\n\n");
-		printf("  Odaberite ulazni format:\n\n");
-		printf("    1.  MPC\n");
-		printf("    2.	SkyMap\n");
-		printf("    3.  Comet for Windows\n");
-		printf("    4.  NASA\n");
-		printf("    5.  Cartes du Ciel/TheSky\n");
-		printf("    6.  Izlaz\n\n");
-		printf("  Izbor: ");
+		printf("\n                                 SSC MAKER\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  Choose input format:\n\n");
+		printf("    1.  MPC                       (Soft00Cmt)\n");
+		printf("    2.	SkyMap                    (Soft01Cmt)\n");
+		printf("    3.  TheSky/Cartes du Ciel     (Soft06Cmt)\n");
+		printf("    4.  Comet for Windows         (Comets.dat)\n");
+		printf("    5.  NASA                      (ELEMENTS.COMET)\n");
+		printf("    6.  NASA                      (sbdb query)\n");
+		printf("    7.  Exit\n\n");
+		printf("  Choice: ");
 		scanf("%d", &a);
-	} while (a<1 || a>6);
+	} while (a<1 || a>7);
 
 	if (a==1){
 		b=input_mpc ();
@@ -46,22 +48,27 @@ int main (){
 		if (b==2) goto end;
 	}
 	else if (a==3){
-		b=input_cfw ();
-		if (b==1) goto start;
-		if (b==2) goto end;
-	}
-	else if (a==4){
-		b=input_nasa ();
-		if (b==1) goto start;
-		if (b==2) goto end;
-	}
-	else if (a==5){
 		b=input_ciel ();
 		if (b==1) goto start;
 		if (b==2) goto end;
 	}
-	else if (a==6)
-		end: printf("\n  Kraj programa...");
+	else if (a==4){
+		b=input_cfw ();
+		if (b==1) goto start;
+		if (b==2) goto end;
+	}
+	else if (a==5){
+		b=input_nasa_elem ();
+		if (b==1) goto start;
+		if (b==2) goto end;
+	}
+	else if (a==6){
+		b=input_nasa_sbdb ();
+		if (b==1) goto start;
+		if (b==2) goto end;
+	}
+	else if (a==7)
+		end: printf("\n  Press any key to exit...");
 
 	getch();
 	return 0;
@@ -92,14 +99,16 @@ int input_mpc (){
 		char nepotrebno [14+1];
 	} comet[MAX];
 
+	fflush(stdin);
+	fflush(stdout);
 	system("CLS");
-	printf("\n     Pretvaram \"MPC format\"\n\n");
-	printf(" ==================================\n\n");
-	printf("    1.   Glavni izbornik\n");
-	printf("    2.   Izlaz\n\n");
-	printf(" ==================================\n\n");
+	printf("\n     Converting \"MPC format\"\n\n");
+	printf(" =============================================================================\n\n");
+	printf("    1.   Main Menu\n");
+	printf("    2.   Exit\n\n");
+	printf(" =============================================================================\n\n");
 
-	unos2: printf("  Unesite ime izvorisne datoteke: ");
+	unos2: printf("  Enter input file name: ");
 	scanf("%s", fin_name);
 
 	b=atoi(fin_name);
@@ -107,13 +116,13 @@ int input_mpc (){
 
 	fin=fopen(fin_name, "r");
 	if (fin==NULL) {
-		printf("\n  Greska pri otvaranju datoteke %s\n\n", fin_name);
+		printf("\n  Error opening file %s\n\n", fin_name);
 		goto unos2;
 	}
 
-	else printf("\n  Datoteka %s je uspjesno otvorena\n", fin_name);
+	else printf("\n  File %s is successfully opened\n", fin_name);
 
-	printf("\n  Unesite ime odredisne datoteke: ");
+	printf("\n  Enter output file name: ");
 	scanf("%s", fout_name);
 
 	b=atoi(fout_name);
@@ -149,11 +158,13 @@ int input_mpc (){
 
 	do {
 		system("CLS");
-		printf("\n  Uspjesno je spremljeno %d kometa u datoteku %s\n\n", N, fout_name);
-		printf(" ==================================\n\n");
-		printf("    1.  Glavni izbornik\n");
-		printf("    2.	Izlaz\n\n");
-		printf("  Izbor: ");
+		printf("\n     Converting \"MPC format\"\n\n");
+		printf(" =============================================================================\n\n");
+		printf("    1.   Main Menu\n");
+		printf("    2.	 Exit\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  %d comets successfully saved in file %s\n\n", N, fout_name);
+		printf("  Choice: ");
 		scanf("%d", &b);
 	} while (b!=1 && b!=2);
 
@@ -183,14 +194,16 @@ int input_skymap (){
 		char nepotrebno [14+1];
 	} comet[MAX];
 
+	fflush(stdin);
+	fflush(stdout);
 	system("CLS");
-	printf("\n     Pretvaram \"SkyMap format\"\n\n");
-	printf(" ==================================\n\n");
-	printf("    1.   Glavni izbornik\n");
-	printf("    2.   Izlaz\n\n");
-	printf(" ==================================\n\n");
+	printf("\n     Converting \"SkyMap format\"\n\n");
+	printf(" =============================================================================\n\n");
+	printf("    1.   Main Menu\n");
+	printf("    2.   Exit\n\n");
+	printf(" =============================================================================\n\n");
 
-	unos2: printf("  Unesite ime izvorisne datoteke: ");
+	unos2: printf("  Enter input file name: ");
 	scanf("%s", fin_name);
 
 	b=atoi(fin_name);
@@ -198,13 +211,13 @@ int input_skymap (){
 
 	fin=fopen(fin_name, "r");
 	if (fin==NULL) {
-		printf("\n  Greska pri otvaranju datoteke %s\n\n", fin_name);
+		printf("\n  Error opening file %s\n\n", fin_name);
 		goto unos2;
 	}
 
-	else printf("\n  Datoteka %s je uspjesno otvorena\n", fin_name);
+	else printf("\n  File %s is successfully opened\n", fin_name);
 
-	printf("\n  Unesite ime odredisne datoteke: ");
+	printf("\n  Enter output file name: ");
 	scanf("%s", fout_name);
 
 	b=atoi(fout_name);
@@ -241,11 +254,13 @@ int input_skymap (){
 
 	do {
 		system("CLS");
-		printf("\n Uspjesno je spremljeno %d kometa u datoteku %s\n\n", N, fout_name);
-		printf(" ==================================\n\n");
-		printf("    1.  Glavni izbornik\n");
-		printf("    2.	Izlaz\n\n");
-		printf("  Izbor: ");
+		printf("\n     Converting \"SkyMap format\"\n\n");
+		printf(" =============================================================================\n\n");
+		printf("    1.   Main Menu\n");
+		printf("    2.	 Exit\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  %d comets successfully saved in file %s\n\n", N, fout_name);
+		printf("  Choice: ");
 		scanf("%d", &b);
 	} while (b!=1 && b!=2);
 
@@ -278,14 +293,16 @@ int input_cfw (){
 		float mag2;
 	} comet[MAX];
 
+	fflush(stdin);
+	fflush(stdout);
 	system("CLS");
-	printf("\n     Pretvaram \"cfw format\"\n\n");
-	printf(" ==================================\n\n");
-	printf("    1.   Glavni izbornik\n");
-	printf("    2.   Izlaz\n\n");
-	printf(" ==================================\n\n");
+	printf("\n     Converting \"cfw format\"\n\n");
+	printf(" =============================================================================\n\n");
+	printf("    1.   Main Menu\n");
+	printf("    2.   Exit\n\n");
+	printf(" =============================================================================\n\n");
 
-	unos1: printf("  Unesite ime izvorisne datoteke: ");
+	unos1: printf("  Enter input file name: ");
 	scanf("%s", fin_name);
 
 	b=atoi(fin_name);
@@ -293,13 +310,13 @@ int input_cfw (){
 
 	fin=fopen(fin_name, "r");
 	if (fin==NULL) {
-		printf("\n  Greska pri otvaranju datoteke %s\n\n", fin_name);
+		printf("\n  Error opening file %s\n\n", fin_name);
 		goto unos1;
 	}
 
-	else printf("\n  Datoteka %s je uspjesno otvorena\n", fin_name);
+	else printf("\n  File %s is successfully opened\n", fin_name);
 
-	printf("\n  Unesite ime odredisne datoteke: ");
+	printf("\n  Enter output file name: ");
 	scanf("%s", fout_name);
 
 	b=atoi(fout_name);
@@ -359,11 +376,13 @@ int input_cfw (){
 
 	do {
 		system("CLS");
-		printf("\n Uspjesno je spremljeno %d kometa u datoteku %s\n\n", N, fout_name);
-		printf(" ==================================\n\n");
-		printf("    1.  Glavni izbornik\n");
-		printf("    2.	Izlaz\n\n");
-		printf("  Izbor: ");
+		printf("\n     Converting \"cfw format\"\n\n");
+		printf(" =============================================================================\n\n");
+		printf("    1.   Main Menu\n");
+		printf("    2.	 Exit\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  %d comets successfully saved in file %s\n\n", N, fout_name);
+		printf("  Choice: ");
 		scanf("%d", &b);
 	} while (b!=1 && b!=2);
 
@@ -371,7 +390,7 @@ int input_cfw (){
 	end: return b;
 }
 
-int input_nasa (){
+int input_nasa_elem (){
 
 	int b, i, N=0;
 	char c, fin_name [80+1], fout_name [80+1];
@@ -394,14 +413,16 @@ int input_nasa (){
 		char nepotrebno [15+1];
 	} comet[MAX];
 
+	fflush(stdin);
+	fflush(stdout);
 	system("CLS");
-	printf("\n     Pretvaram \"NASA format\"\n\n");
-	printf(" ==================================\n\n");
-	printf("    1.   Glavni izbornik\n");
-	printf("    2.   Izlaz\n\n");
-	printf(" ==================================\n\n");
+	printf("\n     Converting \"NASA format\"\n\n");
+	printf(" =============================================================================\n\n");
+	printf("    1.   Main Menu\n");
+	printf("    2.   Exit\n\n");
+	printf(" =============================================================================\n\n");
 
-	unos2: printf("  Unesite ime izvorisne datoteke: ");
+	unos2: printf("  Enter input file name: ");
 	scanf("%s", fin_name);
 
 	b=atoi(fin_name);
@@ -409,13 +430,13 @@ int input_nasa (){
 
 	fin=fopen(fin_name, "r");
 	if (fin==NULL) {
-		printf("\n  Greska pri otvaranju datoteke %s\n\n", fin_name);
+		printf("\n  Error opening file %s\n\n", fin_name);
 		goto unos2;
 	}
 
-	else printf("\n  Datoteka %s je uspjesno otvorena\n", fin_name);
+	else printf("\n  File %s is successfully opened\n", fin_name);
 
-	printf("\n  Unesite ime odredisne datoteke: ");
+	printf("\n  Enter output file name: ");
 	scanf("%s", fout_name);
 
 	b=atoi(fout_name);
@@ -451,11 +472,246 @@ int input_nasa (){
 
 	do {
 		system("CLS");
-		printf("\n Uspjesno je spremljeno %d kometa u datoteku %s\n\n", N, fout_name);
-		printf(" ==================================\n\n");
-		printf("    1.  Glavni izbornik\n");
-		printf("    2.	Izlaz\n\n");
-		printf("  Izbor: ");
+		printf("\n     Converting \"NASA format\"\n\n");
+		printf(" =============================================================================\n\n");
+		printf("    1.   Main Menu\n");
+		printf("    2.	 Exit\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  %d comets successfully saved in file %s\n\n", N, fout_name);
+		printf("  Choice: ");
+		scanf("%d", &b);
+	} while (b!=1 && b!=2);
+
+	fflush(fin);
+	end: return b;
+}
+
+int input_nasa_sbdb (){
+
+	int b, i, j, k, N=0;
+	char c, e, fin_name [80+1], fout_name [80+1];
+	FILE *fin;
+
+	int z1, z2, z3, z4, z5, z6;
+
+	struct data {
+		char name [48+1];
+		int equinox;
+		int epoch_JD;
+		int epoch_y;
+		int epoch_m;
+		int epoch_d;
+		int epoch_h;
+		float peri;
+		float ecc;
+		float peri_node;
+		float asc_node;
+		float incl;
+		float period;
+		char nepotrebno [15+1];
+		char buffer [200+1];
+		char *buff_peri;
+		char *buff_ecc;
+		char *buff_peri_node;
+		char *buff_asc_node;
+		char *buff_incl;
+		char *buff_ey, *buff_em, *buff_ed, *buff_eh;
+	} comet[MAX];
+
+	fflush(stdin);
+	fflush(stdout);
+	system("CLS");
+	printf("\n     Converting \"NASA format\"\n\n");
+	printf(" =============================================================================\n\n");
+	printf("    1.   Main Menu\n");
+	printf("    2.   Exit\n\n");
+	printf(" =============================================================================\n\n");
+
+	unos2: printf("  Enter input file name: ");
+	scanf("%s", fin_name);
+
+	b=atoi(fin_name);
+	if (b==1 || b==2) goto end;
+
+	fin=fopen(fin_name, "r");
+	if (fin==NULL) {
+		printf("\n  Error opening file %s\n\n", fin_name);
+		goto unos2;
+	}
+
+	else printf("\n  File %s is successfully opened\n", fin_name);
+
+	printf("\n  Enter output file name: ");
+	scanf("%s", fout_name);
+
+	b=atoi(fout_name);
+	if (b==1 || b==2) goto end;
+
+	while ((c=fgetc(fin)) != EOF ){
+		if (c=='\n') N++;
+	}
+
+	rewind(fin);
+
+
+	for (i=0; i<N; i++) {
+
+		fscanf(fin, "%200[^\n]%*c", comet[i].buffer);
+
+		{	//ocitavanje zareza u stringu
+			for (j=0; j<200; j++){
+				if (comet[i].buffer[j]==','){
+					z1=j;
+					break;
+				}
+			}
+
+			for (j=z1+1; j<200; j++){
+				if (comet[i].buffer[j]==','){
+					z2=j;
+					break;
+				}
+			}
+
+			for (j=z2+1; j<200; j++){
+				if (comet[i].buffer[j]==','){
+					z3=j;
+					break;
+				}
+			}
+
+			for (j=z3+1; j<200; j++){
+				if (comet[i].buffer[j]==','){
+					z4=j;
+					break;
+				}
+			}
+
+			for (j=z4+1; j<200; j++){
+				if (comet[i].buffer[j]==','){
+					z5=j;
+					break;
+				}
+			}
+
+			for (j=z5+1; j<200; j++){
+				if (comet[i].buffer[j]==','){
+					z6=j;
+					break;
+				}
+			}
+		}
+
+		for (j=0; j<z1; j++){
+			comet[i].name[j]=comet[i].buffer[j];
+		}
+
+		comet[i].name[0]=' ';
+		comet[i].name[z1]='\0';
+
+		{	// premjestanje znakova iz stringa u nove stringove uz
+			// pomoc polozaja zareza i pretvaranje istih u brojeve
+
+			comet[i].buff_peri=malloc(sizeof(char)*(z2-z1));
+			for(j=z1+1, k=0; j<z2; j++, k++)
+				comet[i].buff_peri[k]=comet[i].buffer[j];
+			comet[i].peri=atof(comet[i].buff_peri);
+			free(comet[i].buff_peri);
+
+			comet[i].buff_ecc=malloc(sizeof(char)*(z3-z2));
+			for(j=z2+1, k=0; j<z3; j++, k++)
+				comet[i].buff_ecc[k]=comet[i].buffer[j];
+			comet[i].ecc=atof(comet[i].buff_ecc);
+			free(comet[i].buff_ecc);
+
+			comet[i].buff_peri_node=malloc(sizeof(char)*(z4-z3));
+			for(j=z3+1, k=0; j<z4; j++, k++)
+				comet[i].buff_peri_node[k]=comet[i].buffer[j];
+			comet[i].peri_node=atof(comet[i].buff_peri_node);
+			free(comet[i].buff_peri_node);
+
+			comet[i].buff_asc_node=malloc(sizeof(char)*(z5-z4));
+			for(j=z4+1, k=0; j<z5; j++, k++)
+				comet[i].buff_asc_node[k]=comet[i].buffer[j];
+			comet[i].asc_node=atof(comet[i].buff_asc_node);
+			free(comet[i].buff_asc_node);
+
+			comet[i].buff_incl=malloc(sizeof(char)*(z6-z5));
+			for(j=z5+1, k=0; j<z6; j++, k++)
+				comet[i].buff_incl[k]=comet[i].buffer[j];
+			comet[i].incl=atof(comet[i].buff_incl);
+			free(comet[i].buff_incl);
+
+			comet[i].buff_ey=malloc(sizeof(char)*4);
+			for(j=z6+1, k=0; j<z6+5; j++, k++)
+				comet[i].buff_ey[k]=comet[i].buffer[j];
+			comet[i].epoch_y=atoi(comet[i].buff_ey);
+			free(comet[i].buff_ey);
+
+			comet[i].buff_em=malloc(sizeof(char)*2);
+			for(j=z6+5, k=0; j<z6+7; j++, k++)
+				comet[i].buff_em[k]=comet[i].buffer[j];
+			comet[i].epoch_m=atoi(comet[i].buff_em);
+			free(comet[i].buff_em);
+
+			comet[i].buff_ed=malloc(sizeof(char)*2);
+			for(j=z6+7, k=0; j<z6+9; j++, k++)
+				comet[i].buff_ed[k]=comet[i].buffer[j];
+			comet[i].epoch_d=atoi(comet[i].buff_ed);
+			free(comet[i].buff_ed);
+
+			comet[i].buff_eh=malloc(sizeof(char)*4);
+			for(j=z6+10, k=0; j<z6+14; j++, k++)
+				comet[i].buff_eh[k]=comet[i].buffer[j];
+			comet[i].epoch_h=atoi(comet[i].buff_eh);
+			free(comet[i].buff_eh);
+		}
+
+	/*	// za uklanjanje SOHO kometa
+
+		for (j=0; j<strlen(comet[i].name); j++){
+			if ((comet[i].name[j]  =='S' && comet[i].name[j+1]=='O' &&
+				 comet[i].name[j+2]=='H' && comet[i].name[j+3]=='O')
+				|| (comet[i].peri>=1.5) || (comet[i].ecc >= 1.0)
+				){
+				--i;
+				--N;
+			}
+		}
+	*/
+
+		comet[i].period = compute_period (comet[i].peri, comet[i].ecc);
+		comet[i].epoch_JD = compute_epoch (comet[i].epoch_y, comet[i].epoch_m, comet[i].epoch_d);
+		edit_name (comet[i].name);
+
+	/*	// za uklanjanje kometa ciji je q veci od 1.5 AU
+
+		if (comet[i].peri>=1.5){
+			--i;
+			--N;
+		}
+	*/
+
+		if (comet[i].ecc >= 1.000000)
+			comet[i].ecc = 0.999999;
+	}
+
+	fclose(fin);
+
+	for (i=0; i<N; i++)
+		output (fout_name, comet[i].name, comet[i].period, comet[i].peri, comet[i].ecc,
+			   comet[i].incl, comet[i].asc_node, comet[i].peri_node, comet[i].epoch_JD,
+			   comet[i].epoch_y, comet[i].epoch_m, comet[i].epoch_d, comet[i].epoch_h);
+
+	do {
+		system("CLS");
+		printf("\n     Converting \"NASA format\"\n\n");
+		printf(" =============================================================================\n\n");
+		printf("    1.   Main Menu\n");
+		printf("    2.	 Exit\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  %d comets successfully saved in file %s\n\n", N, fout_name);
+		printf("  Choice: ");
 		scanf("%d", &b);
 	} while (b!=1 && b!=2);
 
@@ -486,14 +742,16 @@ int input_ciel (){
 		char nepotrebno [40+1];
 	} comet[MAX];
 
+	fflush(stdin);
+	fflush(stdout);
 	system("CLS");
-	printf("\n     Pretvaram \"ciel format\"\n\n");
-	printf(" ==================================\n\n");
-	printf("    1.   Glavni izbornik\n");
-	printf("    2.   Izlaz\n\n");
-	printf(" ==================================\n\n");
+	printf("\n     Converting \"ciel format\"\n\n");
+	printf(" =============================================================================\n\n");
+	printf("    1.   Main Menu\n");
+	printf("    2.   Exit\n\n");
+	printf(" =============================================================================\n\n");
 
-	unos2: printf("  Unesite ime izvorisne datoteke: ");
+	unos2: printf("  Enter input file name: ");
 	scanf("%s", fin_name);
 
 	b=atoi(fin_name);
@@ -501,13 +759,13 @@ int input_ciel (){
 
 	fin=fopen(fin_name, "r");
 	if (fin==NULL) {
-		printf("\n  Greska pri otvaranju datoteke %s\n\n", fin_name);
+		printf("\n  Error opening file %s\n\n", fin_name);
 		goto unos2;
 	}
 
-	else printf("\n  Datoteka %s je uspjesno otvorena\n", fin_name);
+	else printf("\n  File %s is successfully opened\n", fin_name);
 
-	printf("\n  Unesite ime odredisne datoteke: ");
+	printf("\n  Enter output file name: ");
 	scanf("%s", fout_name);
 
 	b=atoi(fout_name);
@@ -543,11 +801,13 @@ int input_ciel (){
 
 	do {
 		system("CLS");
-		printf("\n Uspjesno je spremljeno %d kometa u datoteku %s\n\n", N, fout_name);
-		printf(" ==================================\n\n");
-		printf("    1.  Glavni izbornik\n");
-		printf("    2.	Izlaz\n\n");
-		printf("  Izbor: ");
+		printf("\n     Converting \"ciel format\"\n\n");
+		printf(" =============================================================================\n\n");
+		printf("    1.   Main Menu\n");
+		printf("    2.	 Exit\n\n");
+		printf(" =============================================================================\n\n");
+		printf("  %d comets successfully saved in file %s\n\n", N, fout_name);
+		printf("  Choice: ");
 		scanf("%d", &b);
 	} while (b!=1 && b!=2);
 
@@ -586,7 +846,7 @@ char *edit_name (char *name) {
 
 	int i, j, k;
 
-	if (name[0]==' '){          		// ovaj "if" je samo za "nasa format" jer ima razmaka ispred imena kometa
+	if (name[0]==' '){          		// ovaj "if" je samo za "nasa mali format" jer ima razmaka ispred imena kometa
 		for (i=0; i<4; i++) {
 			if (name[i]==' '){
 				for (j=0; name[j+1]!='\0'; j++){
@@ -597,7 +857,7 @@ char *edit_name (char *name) {
 		}
 
 		for (i=0; name[i+1]!='\0'; i++) {
-			if (name[i]==' ' && name[i+1]==' ') {
+			if ((name[i]==' ' && name[i+1]==' ') || (name[i]==name[i+1] && name[i]==name[i+2])) {
 				name[i]='\0';
 				break;
 			}
