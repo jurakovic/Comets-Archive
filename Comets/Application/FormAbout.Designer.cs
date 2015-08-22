b455f95d2707d63eee3313182aabf5eca53fa4c0
jurakovic <17744091+jurakovic@users.noreply.github.com>
2015-08-22 10:07:38 +0000

    About changes

--- Comets/Application/FormAbout.Designer.cs
+++ Comets/Application/FormAbout.Designer.cs
@@ -31,9 +31,8 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
 			this.btnOk = new System.Windows.Forms.Button();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
-			this.label7 = new System.Windows.Forms.Label();
+			this.lblVersion = new System.Windows.Forms.Label();
+			this.lblMore = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -45,7 +44,7 @@
 			this.btnOk.Name = "btnOk";
 			this.btnOk.Size = new System.Drawing.Size(100, 23);
 			this.btnOk.TabIndex = 0;
-			this.btnOk.Text = "Ok";
+			this.btnOk.Text = "OK";
 			this.btnOk.UseVisualStyleBackColor = true;
 			// 
 			// pictureBox1
@@ -57,36 +56,24 @@
 			this.pictureBox1.TabIndex = 1;
 			this.pictureBox1.TabStop = false;
 			// 
-			// label1
+			// lblVersion
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-			this.label1.Location = new System.Drawing.Point(99, 17);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(109, 13);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "Comets 0.8 Beta 4";
+			this.lblVersion.AutoSize = true;
+			this.lblVersion.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.lblVersion.Location = new System.Drawing.Point(99, 17);
+			this.lblVersion.Name = "lblVersion";
+			this.lblVersion.Size = new System.Drawing.Size(109, 13);
+			this.lblVersion.TabIndex = 2;
+			this.lblVersion.Text = "Comets 0.8 Beta 4";
 			// 
-			// label6
+			// lblMore
 			// 
-			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(99, 65);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(39, 13);
-			this.label6.TabIndex = 8;
-			this.label6.Tag = "jurakovic@outlook.com";
-			this.label6.Text = "mailto:";
-			this.label6.Visible = false;
-			// 
-			// label7
-			// 
-			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(99, 41);
-			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(27, 13);
-			this.label7.TabIndex = 7;
-			this.label7.Tag = "Copyright (C) Luka Juraković, 2015";
-			this.label7.Text = "(...)";
+			this.lblMore.AutoSize = true;
+			this.lblMore.Location = new System.Drawing.Point(99, 41);
+			this.lblMore.Name = "lblMore";
+			this.lblMore.Size = new System.Drawing.Size(27, 13);
+			this.lblMore.TabIndex = 7;
+			this.lblMore.Text = "(...)";
 			// 
 			// FormAbout
 			// 
@@ -96,9 +83,8 @@
 			this.CancelButton = this.btnOk;
 			this.ClientSize = new System.Drawing.Size(404, 162);
 			this.ControlBox = false;
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.label7);
-			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblMore);
+			this.Controls.Add(this.lblVersion);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.btnOk);
 			this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
@@ -118,8 +104,7 @@
 
 		private System.Windows.Forms.Button btnOk;
 		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label lblVersion;
+		private System.Windows.Forms.Label lblMore;
 	}
 }
